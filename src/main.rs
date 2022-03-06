ba1e62d3f5c633e1b284824d172a0533de5bbf5d
Floris Vastenhout <f.vastenhout@.live.nl>
2022-03-06 18:19:11 +0100

    formatting, printing, threading

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-//use std::time::Duration;
-//use std::sync::{Arc, Mutex};
-//use std::thread;
-use tungstenite::{connect, Message};
-//use url::Url;
-use serde::{Deserialize, Serialize};
-//use serde_json::{Value};
-use std::collections::HashMap;
-use std::process::exit;
-//use fixed::FixedU64;
-//use fixed::types::extra::U32;
 use rust_decimal::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
+use tungstenite::{connect, Message};
+
+use std::sync::mpsc;
 
 #[derive(Serialize, Deserialize)]
 struct BidAsk {
@@ -17,7 +13,7 @@ struct BidAsk {
     event_type: String,
     #[serde(with = "rust_decimal::serde::float")]
     price: Decimal,
-    amount: f64,
+    amount: f64, //CHANGE TO USIZE
 }
 
 #[derive(Serialize, Deserialize)]
@@ -57,53 +53,84 @@ static REQUEST: &str = r#"
         "id": 0
     }"#;
 
-fn change(ba: BidAsk, map: &mut HashMap<Decimal, f64>) {
+fn change(ba: &BidAsk, map: &mut HashMap<Decimal, f64>) {
     map.insert(ba.price, ba.amount);
 }
 
-fn delete(ba: BidAsk, map: &mut HashMap<Decimal, f64>) {
+fn delete(ba: &BidAsk, map: &mut HashMap<Decimal, f64>) {
     map.remove(&ba.price);
 }
 
-//same as change
-fn new(ba: BidAsk, map: &mut HashMap<Decimal, f64>) {
-    map.insert(ba.price, ba.amount);
-}
-
-fn handle_bid_ask(ba: BidAsk, map: &mut HashMap<Decimal, f64>){
+fn handle_bid_ask(ba: &BidAsk, map: &mut HashMap<Decimal, f64>) {
     match ba.event_type.as_str() {
         "change" => change(ba, map),
         "delete" => delete(ba, map),
-        "new" => new(ba, map),
-        _ => exit(1),
+        "new" => change(ba, map),
+        _ => panic!("API error"),
     }
 }
 
-fn handle(d: Delta, bids: &mut HashMap<Decimal, f64>, asks: &mut HashMap<Decimal, f64>) {
-    for bid in d.params.data.bids {
+fn handle(d: &Delta, bids: &mut HashMap<Decimal, f64>, asks: &mut HashMap<Decimal, f64>) {
+    for bid in &d.params.data.bids {
         handle_bid_ask(bid, bids);
     }
-    for ask in d.params.data.asks {
+    for ask in &d.params.data.asks {
         handle_bid_ask(ask, asks);
     }
+}
+
+fn get_highest(map: &HashMap<Decimal, f64>) -> Decimal {
+    *map.keys()
+        .cloned()
+        .collect::<Vec<Decimal>>()
+        .iter()
+        .max()
+        .unwrap()
+}
+
+fn get_lowest(map: &HashMap<Decimal, f64>) -> Decimal {
+    *map.keys()
+        .cloned()
+        .collect::<Vec<Decimal>>()
+        .iter()
+        .min()
+        .unwrap()
 }
 
 fn main() {
     let mut bids: HashMap<Decimal, f64> = HashMap::new();
     let mut asks: HashMap<Decimal, f64> = HashMap::new();
+    bids.insert(Decimal::new(0, 5), 1.0); //DEBUGGING
+    asks.insert(Decimal::new(9999999, 0), 1.0); //DEBUGGING
+    
+    let (tx, rx) = mpsc::channel();
 
-    let (mut socket, _response) = connect("wss://test.deribit.com/ws/api/v2/").expect("Connection error");
-    socket.write_message(Message::Text(REQUEST.to_string())).unwrap();
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_secs(1));
+            let line = rx.recv().unwrap();
+            println!("{}",line);
+        }
+    });
+
+    let (mut socket, _response) =
+        connect("wss://test.deribit.com/ws/api/v2/").expect("Connection error");
+    socket
+        .write_message(Message::Text(REQUEST.to_string()))
+        .unwrap();
 
     loop {
-        println!("");
         let msg = socket.read_message().expect("Reading error");
-        if &Message::len(&msg) > &150 { // arbitrary number, not interested in the handshake
+        // 150 is high enough to not catch the handshake
+        if &Message::len(&msg) > &150 {
             let parsed: Delta = serde_json::from_str(&Message::into_text(msg).unwrap()).unwrap();
-            handle(parsed, &mut bids, &mut asks);
-            //dbg!(&parsed.params.data.change_id);
+            handle(&parsed, &mut bids, &mut asks);
+            let best_bid = get_highest(&bids);
+            let best_ask = get_lowest(&asks);
+            let line = format!("Best bid price: {0}, count: {1}. Best ask price: {2}, count: {3}",
+                    best_bid, bids[&best_bid], best_ask, asks[&best_ask]
+                    );
+            tx.send(line).unwrap();
         }
-        //dbg!(&bids);
     }
-
 }
