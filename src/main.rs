836d288fec3df1265c104456f657a85ab3833a64
Floris Vastenhout <f.vastenhout@.live.nl>
2022-03-07 16:10:37 +0100

    reconnection after lost delta

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ struct Data {
     #[serde(rename = "type")]
     event_type: String,
     timestamp: usize,
-    prev_change_id: Option<usize>, // the snapshot doesn't have this
+    prev_change_id: Option<usize>,
     instrument_name: String,
     change_id: usize,
     bids: Vec<BidAsk>,
@@ -81,7 +81,7 @@ fn handle(d: &Delta, bids: &mut HashMap<Decimal, f64>, asks: &mut HashMap<Decima
 
 fn get_highest(map: &HashMap<Decimal, f64>) -> Decimal {
     *map.keys()
-        .cloned()
+        .copied()
         .collect::<Vec<Decimal>>()
         .iter()
         .max()
@@ -90,28 +90,16 @@ fn get_highest(map: &HashMap<Decimal, f64>) -> Decimal {
 
 fn get_lowest(map: &HashMap<Decimal, f64>) -> Decimal {
     *map.keys()
-        .cloned()
+        .copied()
         .collect::<Vec<Decimal>>()
         .iter()
         .min()
         .unwrap()
 }
 
-fn main() {
+fn read_socket(tx: &mpsc::Sender<String>) {
     let mut bids: HashMap<Decimal, f64> = HashMap::new();
     let mut asks: HashMap<Decimal, f64> = HashMap::new();
-    bids.insert(Decimal::new(0, 5), 1.0); //DEBUGGING
-    asks.insert(Decimal::new(9999999, 0), 1.0); //DEBUGGING
-    
-    let (tx, rx) = mpsc::channel();
-
-    thread::spawn(move || {
-        loop {
-            thread::sleep(Duration::from_secs(1));
-            let line = rx.recv().unwrap();
-            println!("{}",line);
-        }
-    });
 
     let (mut socket, _response) =
         connect("wss://test.deribit.com/ws/api/v2/").expect("Connection error");
@@ -119,18 +107,42 @@ fn main() {
         .write_message(Message::Text(REQUEST.to_string()))
         .unwrap();
 
+    socket
+        .read_message()
+        .expect("Error reading subscription confirmation");
+    let msg = socket.read_message().expect("Reading error");
+    let parsed: Delta = serde_json::from_str(&Message::into_text(msg).unwrap()).unwrap();
+    handle(&parsed, &mut bids, &mut asks);
+    let mut old_id: Option<usize> = Some(parsed.params.data.change_id);
+
     loop {
         let msg = socket.read_message().expect("Reading error");
-        // 150 is high enough to not catch the handshake
-        if &Message::len(&msg) > &150 {
-            let parsed: Delta = serde_json::from_str(&Message::into_text(msg).unwrap()).unwrap();
-            handle(&parsed, &mut bids, &mut asks);
-            let best_bid = get_highest(&bids);
-            let best_ask = get_lowest(&asks);
-            let line = format!("Best bid price: {0}, count: {1}. Best ask price: {2}, count: {3}",
-                    best_bid, bids[&best_bid], best_ask, asks[&best_ask]
-                    );
-            tx.send(line).unwrap();
+        let parsed: Delta = serde_json::from_str(&Message::into_text(msg).unwrap()).unwrap();
+        if parsed.params.data.prev_change_id != old_id {
+            break;
+        } else {
+            old_id = Some(parsed.params.data.change_id);
         }
+        handle(&parsed, &mut bids, &mut asks);
+        let best_bid = get_highest(&bids);
+        let best_ask = get_lowest(&asks);
+        let line = format!(
+            "Best bid price: {0}, count: {1}. Best ask price: {2}, count: {3}",
+            best_bid, bids[&best_bid], best_ask, asks[&best_ask]
+        );
+        tx.send(line).unwrap();
     }
+    read_socket(&tx);
+}
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(1));
+        let line = rx.recv().unwrap();
+        println!("{}", line);
+    });
+
+    read_socket(&tx);
 }
